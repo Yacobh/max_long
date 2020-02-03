ac3e40a28d2343c8483c1c71849b7909222c6c9c
yacobh <jacobocordova@gmail.com>
2020-02-02 23:55:11 -0300

    Initial Commit

---
+++ max_long/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  max_long
+//
+//  Created by Yacobh on 2/2/20.
+//  Copyright © 2020 Yacobh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
